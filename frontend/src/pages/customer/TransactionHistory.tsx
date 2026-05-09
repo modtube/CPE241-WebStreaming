bfa0b38ba6d0ca66316d33d4cd506a03cf6a8779
Piink7878 <147244759+Piink7878@users.noreply.github.com>
2026-05-09 08:10:13 +0700

    Transaction History

--- frontend/src/pages/customer/TransactionHistory.tsx
+++ frontend/src/pages/customer/TransactionHistory.tsx
@@ -1,14 +1,82 @@
+// using component ReceiptModal.tsx, TransactionHistoryTable.tsx
+
+import TransactionHistoryTable, { type TransactionRecord } from "../../components/customer/TransactionHistoryTable";
+
+const mockData: TransactionRecord[] = [
+  {
+    transactionId: "TXN-20250422",
+    date: "2015-12-11",
+    movieTitle: "Echoes of Tomorrow, Hollow Crown",
+    amount: "$10.48",
+    payment: "Credit Card",
+    status: "Completed",
+    receipt: {
+      transactionId: "TXN-20250422",
+      items: [
+        { title: "Echoes of Tomorrow", price: 6.99 },
+        { title: "Hollow Crown", price: 3.49 },
+      ],
+      subtotal: 10.48,
+      discount: 0,
+      total: 10.48,
+      date: "2020-12-11",
+      paymentMethod: "Credit Card",
+      status: "Completed",
+    },
+  },
+  {
+    transactionId: "TXN-20250423",
+    date: "2026-12-11",
+    movieTitle: "Echoes of Tomorrow, Hollow Crown",
+    amount: "$10.48",
+    payment: "Credit Card",
+    status: "Completed",
+    receipt: {
+      transactionId: "TXN-20250423",
+      items: [
+        { title: "Echoes of Tomorrow", price: 6.99 },
+        { title: "Hollow Crown", price: 3.49 },
+      ],
+      subtotal: 10.48,
+      discount: 0,
+      total: 10.48,
+      date: "2020-12-11",
+      paymentMethod: "Credit Card",
+      status: "Completed",
+    },
+  },
+  {
+    transactionId: "TXN-20250424",
+    date: "2020-12-11",
+    movieTitle: "Echoes of Tomorrow, Hollow Crown",
+    amount: "$10.48",
+    payment: "Credit Card",
+    status: "Completed",
+    receipt: {
+      transactionId: "TXN-20250424",
+      items: [
+        { title: "Echoes of Tomorrow", price: 6.99 },
+        { title: "Hollow Crown", price: 3.49 },
+      ],
+      subtotal: 10.48,
+      discount: 0,
+      total: 10.48,
+      date: "2020-12-11",
+      paymentMethod: "Credit Card",
+      status: "Completed",
+    },
+  },
+
+]
+
 export default function TransactionHistory() {
   return (
-    <div className="min-h-screen w-full bg-white rounded-3xl shadow-2xl p-6">
-      <div className="text-center py-12">
-        <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-          Transaction History
-        </h2>
-        <p className="text-gray-600">
-          View and export all of your transactions.
-        </p>
+    <div className="min-h-screen w-full bg-black text-white p-8 flex flex-col gap-y-6">
+      <div className="flex flex-col gap-y-1"> 
+        <h4 className="text-2xl font-bold">Transaction History</h4>
+        <p className="text-[#9CA3AF] text-sm font-normal">All your purchases</p>
       </div>
+      <TransactionHistoryTable data={mockData} />
     </div>
   );
 }
