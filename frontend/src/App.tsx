4424a1b1bcb54d6cb6addf44baaf0b9aea2d86ea
osen11286 <137164797+osen11286@users.noreply.github.com>
2026-05-11 16:19:28 +0700

    Add TransactionDetails,transactionController,transactionRoutes and Edit navbar ,app.tsx,index.ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,3 @@
-// src/App.tsx
-// src/App.tsx
 import { Routes, Route, Navigate } from "react-router-dom";
 import AdminLayout from "./layouts/AdminLayout";
 import AuthLayout from "./layouts/AuthLayout";
@@ -13,12 +11,13 @@ import CrewView from "./pages/admin/Crew/Crew";
 import CrewForm from "./pages/admin/Crew/CrewForm";
 import UserManagement from "./pages/admin/User/User";
 import TransactionsView from "./pages/admin/Transaction/Transactions";
+import TransactionDetail from "./pages/admin/Transaction/TransactionDetail";
 import SetupsView from "./pages/admin/Setup/Setups";
 import Home from "./pages/customer/Home";
 import PersonalLibrary from "./pages/customer/PersonalLibrary";
 import Playlists from "./pages/customer/Playlists";
 import TransactionHistory from "./pages/customer/TransactionHistory";
-import EditProfile from "./pages/customer/EditProfile"
+import EditProfile from "./pages/customer/EditProfile";
 import GenreView from "./pages/admin/Setup/Genre";
 import LanguageView from "./pages/admin/Setup/Language";
 import CountryView from "./pages/admin/Setup/Country";
@@ -27,25 +26,32 @@ import RatingView from "./pages/admin/Setup/Rating";
 function App() {
   return (
     <Routes>
-      {/* Public auth pages */}
+      {/* ===== Auth ===== */}
       <Route path="/" element={<AuthLayout />}>
         <Route index element={<Login />} />
         <Route path="login" element={<Login />} />
       </Route>
 
-      {/* Admin Routes */}
+      {/* ===== Admin ===== */}
       <Route path="/admin" element={<AdminLayout />}>
         <Route index element={<Navigate to="dashboard" replace />} />
         <Route path="dashboard" element={<AdminDashboard />} />
+
         <Route path="movies" element={<MoviesView />} />
         <Route path="movies/add" element={<MoviesForm />} />
         <Route path="movies/edit/:id" element={<MoviesForm />} />
+
         <Route path="reviews" element={<ReviewsView />} />
+
         <Route path="crew" element={<CrewView />} />
         <Route path="crew/add" element={<CrewForm />} />
-        <Route path="crew/edit/:id" element={<CrewForm />} />        
+        <Route path="crew/edit/:id" element={<CrewForm />} />
+
         <Route path="users" element={<UserManagement />} />
+
         <Route path="transactions" element={<TransactionsView />} />
+        <Route path="transactions/:id" element={<TransactionDetail />} />
+
         <Route path="setups" element={<SetupsView />} />
         <Route path="setups/genre" element={<GenreView />} />
         <Route path="setups/language" element={<LanguageView />} />
@@ -53,7 +59,7 @@ function App() {
         <Route path="setups/rating" element={<RatingView />} />
       </Route>
 
-      {/* Customer Routes */}
+      {/* ===== Customer ===== */}
       <Route path="/customer" element={<CustomerLayout />}>
         <Route index element={<Navigate to="home" replace />} />
         <Route path="home" element={<Home />} />
@@ -68,4 +74,5 @@ function App() {
     </Routes>
   );
 }
+
 export default App;
